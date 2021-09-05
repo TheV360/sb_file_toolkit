3fb6a9bea3d5003b59b4bfa09eb5cc21e71e04bf
V360 <v360code@gmail.com>
2021-09-05 01:00:17 -0400

    or how about argh

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::fs;
-use std::path::Path;
+use std::path::PathBuf;
 use std::io::Write;
 use std::ffi;
 
-use clap::{App, Arg, crate_version, crate_authors, crate_description};
+use argh::FromArgs;
 
 // This syntax looks antiquated (i.e. from Rust 2015)
 // but no, this *is* how you include macros.
@@ -14,32 +14,40 @@ extern crate log;
 mod sb_file;
 
 fn main() {
-	let matches = App::new("SmileBASIC CLI Tools")
-		.version(crate_version!())
-		.author(crate_authors!(", "))
-		.about(crate_description!())
-		.arg(Arg::new("input")
-			.about("The input file to use.")
-			.index(1)
-			.required(true)
-		)
-		.arg(Arg::new("output")
-			.about("The output file to write.")
-			.index(2)
-			.required(true)
-			// FIXME: would be nice if the output name was generated if not present
-			// i.e. prg.txt / prg.prg / prg.sb / prg.sb3 / prg.sb4 => TPRG
-			// i.e. sprites.png / sprites.bmp => BSPRITES
-			// i.e. TPRG => prg.sb3 / prg.sb4 (opinionated file ext.s) (dependant on FileVersion)
-			// i.e. BSPRITES => sprites.png
-		)
-		.get_matches();
+	// FIXME: would be nice if the output name was generated if not present
+	// i.e. prg.txt / prg.prg / prg.sb / prg.sb3 / prg.sb4 => TPRG
+	// i.e. sprites.png / sprites.bmp => BSPRITES
+	// i.e. TPRG => prg.sb3 / prg.sb4 (opinionated file ext.s) (dependant on FileVersion)
+	// i.e. BSPRITES => sprites.png
+	
+	/* To Clap...
+	.version(crate_version!())
+	.author(crate_authors!(", "))
+	.about(crate_description!())
+	...I'll miss you. */
+	
+	#[derive(FromArgs)]
+	/// SmileBASIC File Toolkit
+	///
+	/// Tool to read/write SmileBASIC files in the command line.
+	/// (Currently only supports SB3 TXT/PRG files)
+	struct FileArgs {
+		/// the input file to use
+		#[argh(positional)]
+		input: PathBuf,
+		
+		/// the output file to write
+		#[argh(positional)]
+		output: PathBuf,
+	}
+	
+	let fa: FileArgs = argh::from_env();
 	
 	env_logger::init();
 	
 	// https://doc.rust-lang.org/std/path/struct.PathBuf.html
-	let input_file = Path::new(matches.value_of("input").unwrap());
-	let output_file = Path::new(matches.value_of("output").unwrap());
+	let input_file = &fa.input;
+	let output_file = &fa.output;
 	
 	let mut file_text = fs::read_to_string(input_file).expect("sorry");
 	info!("Loaded SmileBASIC 3 source file from `{:?}`", input_file);
